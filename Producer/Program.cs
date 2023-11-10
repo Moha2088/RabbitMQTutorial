243c9a52ae87353bc8cf9a03864abc78466f91fa
Mohamed Shil <maxamed-14@outlook.dk>
2023-11-10 11:18:03 +0100

    Initial Commit

---
+++ Producer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost", Port = 6000 };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+
+var routingKey = "hello.pdf";
+var message = "Hello";
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(exchange: "topic_logs",
+                     routingKey: routingKey,
+                     basicProperties: null,
+                     body: body);
+Console.WriteLine($" [x] Sent '{routingKey}':'{message}'");
